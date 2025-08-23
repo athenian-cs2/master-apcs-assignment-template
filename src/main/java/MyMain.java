5fa1604189dc4a612443c858e5d09b1e176da93f
sthompson-athenian <sthompson@athenian.org>
2025-08-22 17:34:57 -0700

    Update to Java 21 LTS, Gradle 8.10, and modern dependencies with code formatting

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,7 +1,7 @@
 public class MyMain {
-    public static void main(String[] args) {
-        MyClass myClass = new MyClass("Hello World!");
+  public static void main(String[] args) {
+    MyClass myClass = new MyClass("Hello World!");
 
-        System.out.println(myClass.getGreeting());
-    }
+    System.out.println(myClass.getGreeting());
+  }
 }
